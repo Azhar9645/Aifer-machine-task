d51361b68e2d98da34545708e518fa43afc82a86
Azharudheen <azharudheenasru@gmail.com>
2025-01-07 10:51:28 +0530

    Updated README with APK link and screenshots

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -48,31 +48,37 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text('Wallpaper App'),
         centerTitle: true,
       ),
-      body: RefreshIndicator(
-        onRefresh: _onRefresh,
-        child: BlocBuilder<PhotoBloc, PhotoState>(
-          builder: (context, state) {
-            if (state is PhotoLoadingState && _page == 1) {
+      body: BlocListener<PhotoBloc, PhotoState>(
+        listener: (context, state) {
+          if (state is PhotoSuccessState || state is PhotoErrorState) {
+            _isFetching = false; // Reset fetching state after loading
+          }
+        },
+        child: RefreshIndicator(
+          onRefresh: _onRefresh,
+          child: BlocBuilder<PhotoBloc, PhotoState>(
+            builder: (context, state) {
+              if (state is PhotoLoadingState && _page == 1) {
+                return ShimmerGrid();
+              } else if (state is PhotoErrorState) {
+                return Center(child: Text(state.message));
+              } else if (state is PhotoSuccessState) {
+                return PhotoGrid(
+                  scrollController: _scrollController,
+                  photos: state.photos,
+                  onImageTap: _showImageDownloadDialog,
+                );
+              } else if (state is PhotoEmptyState) {
+                return const Center(child: Text('No more photos.'));
+              }
               return ShimmerGrid();
-            } else if (state is PhotoErrorState) {
-              return Center(child: Text(state.message));
-            } else if (state is PhotoSuccessState) {
-              return PhotoGrid(
-                scrollController: _scrollController,
-                photos: state.photos,
-                onImageTap: _showImageDownloadDialog,
-              );
-            } else if (state is PhotoEmptyState) {
-              return const Center(child: Text('No more photos.'));
-            }
-            return ShimmerGrid();
-          },
+            },
+          ),
         ),
       ),
     );
   }
 
-  // Fetch more photos when the user scrolls to the bottom
   void _onScroll() {
     if (_scrollController.position.pixels ==
             _scrollController.position.maxScrollExtent &&
@@ -85,28 +91,27 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  // Handle pull-to-refresh
   Future<void> _onRefresh() async {
     setState(() {
-      _page = 1; 
+      _page = 1;
     });
 
     context.read<PhotoBloc>().add(ClearPhotos());
-    context.read<PhotoBloc>().add(RefreshPhotos(photoPerPage: _photosPerPage));
+    context
+        .read<PhotoBloc>()
+        .add(RefreshPhotos(photoPerPage: _photosPerPage));
 
     await Future.delayed(const Duration(seconds: 1));
     _isFetching = false;
   }
 
-  // Show dialog to download image
   void _showImageDownloadDialog(String imageUrl) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           title: const Text("Download Image"),
-          content:
-              const Text("Do you want to download this image to your gallery?"),
+          content: const Text("Do you want to download this image to your gallery?"),
           actions: <Widget>[
             TextButton(
               onPressed: () {
